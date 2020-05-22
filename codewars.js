06a53c5254351d015f2ae370ee47f44ea311260e
nick <56331108+nick-ball@users.noreply.github.com>
2020-05-22 22:24:12 +0000

    Create codewars.js

---
+++ codewars.js
@@ -0,0 +1,20 @@
+//codewars     nickball
+//edabit       nickball
+
+//"Achieve mastery through challenge"
+//custom fizzbuzz
+var fizzBuzzCustom = function(s1 = "Fizz", s2 = "Buzz", n1 = 3, n2 = 5) {
+  let arr = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % n1 === 0 && i % n2 === 0) {
+      arr.push(s1 + s2);
+    } else if (i % n1 === 0) {
+      arr.push(s1);
+    } else if (i % n2 === 0) {
+      arr.push(s2);
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
