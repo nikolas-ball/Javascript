20bd4d78a0cd556f0e4a662d22ef788ba1066974
nick <56331108+nick-ball@users.noreply.github.com>
2020-05-26 18:53:18 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -30,3 +30,20 @@ var array = [1, 2, 3, 4, 5];
     }, 0);
     
     console.log(sum); // Prints: 15
+
+
+
+// Bit Counting
+// http://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
+ 
+var countBits = function(n) {
+    var count = 0;
+    while (n > 0) {
+        if (n & 1 === 1)
+            count++;
+        n >>= 1;
+    }
+    return count;
+};
+
+console.log(countBits(1234));
