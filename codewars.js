a1fb05b03f3bad40c9d5dca8e4b77b18dfe25f7d
nick <56331108+nick-ball@users.noreply.github.com>
2020-05-30 00:11:13 +0000

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -60,3 +60,35 @@ function isRepdigit(num) {
 	return false;
 	
 }
+
+
+//Hoisting
+//What’s the result of executing this code and why
+function test() {
+   console.log(a);
+   console.log(foo());
+   
+   var a = 1;
+   function foo() {
+      return 2;
+   }
+}
+
+//The result of this code is undefined and 2
+
+//The reason is that both variables and functions are hoisted (moved at the top of the function) but variables don’t retain any assigned value. So, at the time the variable a is printed, it exists in the function (it’s declared) but it’s still undefined. Stated in other words, the code above is equivalent to the following:
+
+function test() {
+   var a;
+   function foo() {
+      return 2;
+   }
+
+   console.log(a);
+   console.log(foo());
+   
+   a = 1;
+}
+
+test();
+//end hoisting
