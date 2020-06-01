cdbc893e7f23caf4b1462d8030d5944719549429
Nick Ball <ballnikolas@yahoo.com>
2020-06-01 19:57:34 -0400

    DOM2

--- dom.js
+++ dom.js
@@ -1,1 +1,4 @@
 //a taste of the browser DOM 
+
+//The document object is our entry point into the world of the DOM.
+//It contains representations of all the content on a page, plus tons of useful methods and properties.
