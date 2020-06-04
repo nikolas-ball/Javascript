96fdf69b9e0aec48447062955f39ea77e8006be3
nick <56331108+nick-ball@users.noreply.github.com>
2020-06-03 23:46:18 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -111,3 +111,31 @@ bar(); // Calling foo function will not give an Error
 //</script>
 
 //Another advantage of this first-one way of declaration is that you can declare functions based on certain conditions
+
+
+//return number as a array with every number leading up to number(including the number itself) that is divisble by 4, multiplied by 10.
+//amplify(4) ➞ [1, 2, 3, 40]
+// Create a sequence from 1 to 4
+// 4 is exactly divisible by 4, so it will be 4*10 = 40
+
+//amplify(3) ➞ [1, 2, 3]
+// Create a sequence from 1 to 3
+// There are no numbers that can be exactly divided by 4
+
+//amplify(25) ➞ [1, 2, 3, 40, 5, 6, 7, 80, 9, 10, 11, 120, 13, 14, 15, 160, 17, 18, 19, 200, 21, 22, 23, 240, 25]
+// Create a sequence from 1 to 25
+// The numbers exactly divisible by 4 are: 4 (4*10 = 40), 8 (8 * 10 = 80)... and so on.
+
+function amplify(num) {
+	var a = [];
+	for(var i = 1; i <= num; i++){
+		if(i % 4 == 0){
+			a.push(i*10);
+		}else{
+			a.push(i);
+		}
+	}
+	return a;
+}
+amplify(25);
+//==============================================================================================================
