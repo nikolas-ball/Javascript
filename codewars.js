2149a9a84a3183f12aa422e8611b78c30aee65d6
nick <56331108+nick-ball@users.noreply.github.com>
2020-06-09 18:25:10 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -468,3 +468,28 @@ console.log(bst2.convertToLinkedList());
 //{ head: { value: 3, next: { value: 5, next: [Object] } } }
 
 //===================================================================================================================================================================================
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+    
+    var bmi = weight % (Math.pow(height, 2));
+
+    if (bmi <= 18.5) {
+        return "Underweight"
+    } else if (18.5 < bmi <= 25.0) {
+        return "Normal"
+    } else if (25.0 < bmi <= 30.0){
+        return "Overweight"
+    }else if(bmi > 30){
+        return "Obese"
+    }
+}
+
+bmi(80, 1.80);
+//=============================================================================================
