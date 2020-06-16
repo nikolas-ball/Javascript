95b88e30c1e03cf3624570ddd077c25f8abec0d8
nick <56331108+nick-ball@users.noreply.github.com>
2020-06-15 22:32:35 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -531,3 +531,23 @@ bmi(80, 1.80);
 
 //==
 //=======================================================================================
+<script type="text/javascript">
+
+var images = new Array();
+
+function preloadImages(){
+
+    for (i=0; i < preloadImages.arguments.length; i++){
+
+         images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
+
+}
+
+preloadImages("logo.jpg", "main_bg.jpg", "body_bg.jpg", "header_bg.jpg");
+
+</script>
+//=======================================================================================
