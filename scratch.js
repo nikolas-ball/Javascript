d5e38c752047e256be45806039a65ddbbc9348cf
nick <56331108+nick-ball@users.noreply.github.com>
2020-06-22 23:58:20 -0400

    Update scratch.js

--- scratch.js
+++ scratch.js
@@ -17,3 +17,18 @@ const fizzGame = num => {
 }
 
 //more practice 
+const fizzGame = num => {
+    for(let i = 0; i <= num; i++){
+        let output = '';
+        if(i % 3 === 0){
+            output += 'Fizz';
+        }
+        if(i % 5 === 0){
+            output += 'Buzz';
+        }
+        if(output === ''){
+            output = i;
+        }
+        console.log(output);
+    }
+}
