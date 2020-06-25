7a19339c79bd2d788b6ca41c0526fb3f954d8c56
nick <56331108+nick-ball@users.noreply.github.com>
2020-06-24 23:23:54 -0400

    Update scratch.js

--- scratch.js
+++ scratch.js
@@ -32,3 +32,11 @@ const fizzGame = num => {
         console.log(output);
     }
 }
+
+//=========================================== simpler solution but same concept
+for (let i = 1; i <= 100; i++) {
+    let out = '';
+    if (i % 3 === 0) out += 'Fizz';
+    if (i % 5 === 0) out += 'Buzz';
+    console.log(out || i);
+}
